48abee0933e018a273555d77609ccd56844d796f
suleyman2032 <140537145+suleyman2032@users.noreply.github.com>
2023-08-09 01:26:54 +0300

    Create zar_atma.py

---
+++ zar_atma.py
@@ -0,0 +1,15 @@
+import random
+def zar_atma():
+    zar = random.randint(0, 5)
+    if zar == 0:
+        return "1"
+    elif zar == 1:
+        return "2"
+    elif zar == 2:
+        return "3"
+    elif zar == 3:
+        return "4"
+    elif zar == 4:
+        return "5"
+    else:
+        return"6"
