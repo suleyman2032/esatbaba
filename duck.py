8960736b2bdb301f5434129fc742bd8948f53fb4
suleyman2032 <140537145+suleyman2032@users.noreply.github.com>
2023-08-22 11:22:18 +0300

    Create duck.py

---
+++ duck.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+from aiohttp import request
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = request.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command("duck")
+async def duck(ctx):
+    #'''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+bot.run("MTEzNjY5ODE5MjE2ODIzMDkyMg.GWqjR6.wrcB1JlfKfDbphqkHiUSqQj2XACICkl1rk-A18")
